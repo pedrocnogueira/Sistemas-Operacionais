25c14e4d9ce3b78a1c16ec89318fd92d1c7dc19d
pedro.nogueira <pedro.crnogueira@gmail.com>
2025-09-25 10:09:34 -0300

    Questão 1 do Lab04 feita em dois arquivos

---
+++ Lab04/ctrl-c.c
@@ -0,0 +1,30 @@
+// ctrl-c.c
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EVER for (;;)
+
+void intHandler(int sinal) {
+  printf("Voce pressionou Ctrl-C (SIGINT=%d)\n", sinal);
+  fflush(stdout);
+}
+
+void quitHandler(int sinal) {
+  printf("Recebi Ctrl-\\ (SIGQUIT=%d). Terminando o processo...\n", sinal);
+  fflush(stdout);
+  exit(0);
+}
+
+int main(void) {
+  void (*p)(int);
+
+  p = signal(SIGINT, intHandler);
+  printf("Endereco do manipulador anterior de SIGINT: %p\n", (void *)p);
+
+  p = signal(SIGQUIT, quitHandler);
+  printf("Endereco do manipulador anterior de SIGQUIT: %p\n", (void *)p);
+
+  puts("Ctrl-C tratado (nao termina). Use Ctrl-\\ para terminar.");
+  EVER;
+}
